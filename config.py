376b60226444ad76c849a78a3787bd8f3a41afd5
jang1601uw <jang1601@uw.edu>
2026-05-28 23:05:03 -0700

    [jang] change back flat obs size to all obst

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ WORLD_SIZE = 10.0
 NUM_AGENTS = 1
 # goal_pos(2) + obstacles(3, closest only) + robot_pos(2) + robot_vel(2)
 # + other_agents((NUM_AGENTS-1)*3)
-FLATTENED_OBS_SIZE = 2 + 3 + 2 + 2 + (NUM_AGENTS - 1) * 3
+FLATTENED_OBS_SIZE = 2 + (NUM_OBSTACLES * 3) + 2 + 2 + (NUM_AGENTS - 1) * 3
 
 # --- Configuration Definition ---
 cfg = {
